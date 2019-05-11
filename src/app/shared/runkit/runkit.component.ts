d18dd0550999be8fbec15236c822c46f89973ce3
JBetoReyes <josereyes55@hotmail.com>
2019-05-11 18:35:43 -0500

    adds the runkit component

---
+++ src/app/shared/runkit/runkit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-runkit',
+  templateUrl: './runkit.component.html',
+  styleUrls: ['./runkit.component.scss']
+})
+export class RunkitComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/shared/runkit/runkit.module.ts
+++ src/app/shared/runkit/runkit.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RunkitComponent } from './runkit.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [RunkitComponent],
   imports: [
     CommonModule
   ]
